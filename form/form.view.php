b25614bb6c9b711ca692b36a53100dee4dea95ba
UnderVarela <under.varela@gmail.com>
2023-04-03 16:25:19 +0200

    Validando el formulario. Mostrar errores. Incompleto.

--- form/form.view.php
+++ form/form.view.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-  <link rel="stylesheet" href="form/style.css">
-  <title>Envío de formulario</title>
-</head>
-
-<body class="body">
   <main class="container">
     <h1 class="title">Enviar mensaje</h1>
     <form method="post">
@@ -18,30 +6,32 @@
         <label for="nombre-completo">
           Nombre Completo <span class="required">*</span>
         </label>
-        <input name="nombre-completo" id="nombre-completo" type="text" placeholder="Perico de los palotes">
+        <input value="<?=$_POST['nombre-completo']??''?>" 
+        name="nombre-completo" id="nombre-completo" type="text" placeholder="Perico de los palotes">
       </div>
       <div class="field">
         <label for="correo">
           Correo Electronico <span class="required">*</span>
         </label>
-        <input name="correo" id="correo" type="text" placeholder="pericos@palotes.com">
+        <input 
+        value="<?=$_POST['correo']??''?>"
+        name="correo" id="correo" type="text" placeholder="pericos@palotes.com">
       </div>
       <div>
         <label for="mensaje">
           Mensaje <span class="required">*</span>
         </label>
-        <textarea name="mensaje" id="mensaje"></textarea>
+        <textarea name="mensaje" id="mensaje" placeholder="Escriba un mensaje"><?=$_POST['mensaje']??''?></textarea>
       </div>
       <div>
-        <input type="checkbox" class="politica-privacidad" name="politica-privacidad" id="politica-privacidad">
+        <input 
+        <?=(isset($_POST['politica-privacidad']) && $_POST['politica-privacidad']==="on")?'checked' : ''?> 
+        type="checkbox" class="politica-privacidad" name="politica-privacidad" id="politica-privacidad">
         <label>
           Acepto la politica de privacidad <span class="required">*</span>
         </label>
       </div>
+      <?=$errores ? $errores : ''?>
       <button class="btn-enviar">Enviar</button>
     </form>
   </main>
-
-</body>
-
-</html>
--- index-X.php
+++ index-X.php
@@ -1,2 +1,45 @@
 <?php
+
+// Fichero de configuración
+// echo "<pre>";
+// print_r($_POST);
+// echo "<pre>";
+
+// Validaciones. No podemos usar extract() en este caso porque los name usan guiones.
+$errores = '';
+
+if(isset($_POST['btn-enviar'])) {
+  
+  // nombre
+ $nombre = $_POST['nombre-completo'];
+ $nombre = trim($nombre);
+ $nombre = htmlspecialchars($nombre);
+ if (strlen($nombre) === 0) {
+  $errores.='<strong class="error">El campo nombre no puede estar vacío</strong>';
+ }
+
+  // correo
+  $correo = $_POST['correo'];
+  $correo = trim($correo);
+  $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+  if (strlen($correo) === 0) {
+   $errores.='<strong class="error">El campo correo no es válido</strong>';
+  } else if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+    $errores.=sprintf('El correo <mark>%s</mark> no es válido',$correo);
+  }
+
+  
+  
+// mensaje
+ $mensaje = $_POST['mensaje'];
+ $mensaje = trim($mensaje);
+ $mensaje = htmlspecialchars($mensaje);
+ if (strlen($mensaje) === 0) {
+  $errores.='<strong class="error">El campo mensaje no puede estar vacío</strong>';
+ }
+}
+
+// Vistas
+require_once('./header.php');
 require_once('./form/form.view.php');
+require_once('./footer.php');
